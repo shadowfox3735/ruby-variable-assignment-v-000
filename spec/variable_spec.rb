1fb9db5296a6327591a4e11dfd4ba777b13f941e
shadowfox3735 <normanpan180@gmail.com>
2018-10-02 21:46:43 +0000

    automatically backed up by learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -2,8 +2,6 @@
 
 describe "./variable.rb" do
 
-greeting = "Hello World"
-
   it "defined a local variable called greeting and set it equal to 'Hello World'" do
     greeting = get_variable_from_file('./variable.rb', "greeting")
 
